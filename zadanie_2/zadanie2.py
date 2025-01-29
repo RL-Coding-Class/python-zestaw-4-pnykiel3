6380c4512e19762bc20a00138d0496016e0b9b8e
Paweł Nykiel <162437174+pnykiel3@users.noreply.github.com>
2025-01-29 19:00:41 +0100

    Update zadanie2.py
    Signed-off-by: Paweł Nykiel <162437174+pnykiel3@users.noreply.github.com>

--- zadanie_2/zadanie2.py
+++ zadanie_2/zadanie2.py
@@ -6,36 +6,78 @@ class Pojazd(ABC):
         self._rok = rok
         self._predkosc = 0
 
-    # Dokoncz definicje, rowniez setter i deleter
-    # @property
-    # def predkosc(self) -> float:
+    @property
+    def predkosc(self) -> float:
+        return self._predkosc
 
+    @predkosc.setter
+    def predkosc(self, v: float):
+        if v < 0:
+            raise ValueError("Prędkość nie może być ujemna!")
+        self._predkosc = v
+
+    @predkosc.deleter
+    def predkosc(self):
+        self._predkosc = 0
 
 class Samochod(Pojazd):
-# w __init__ dodaj skladowa liczba_drzwi
+    def __init__(self, model: str, rok: int, liczba_drzwi: int):
+        super().__init__(model, rok)
+        self.liczba_drzwi = liczba_drzwi
 
 class Autobus(Pojazd):
-# w __init__ dodaj skladowa liczba_miejsc
-
+    def __init__(self, model: str, rok: int, liczba_miejsc: int):
+        super().__init__(model, rok)
+        self.liczba_miejsc = liczba_miejsc
 
 class FabrykaPojazdow(ABC):
     def __init__(self, nazwa: str):
         self._nazwa = nazwa
         self._liczba_wyprodukowanych = 0
 
-    # do uzupelnienia rozne metody jak na diagramie i w opisie
+    @property
+    def nazwa (self) -> str:
+        return self._nazwa
+
+    @classmethod
+    def utworz_fabryke(cls, typ: str, nazwa: str):
+        if typ == 'autobus':
+            return FabrykaAutobusow(nazwa)
+        elif typ == 'samochod':
+            return FabrykaSamochodow(nazwa)
+        else:
+            raise ValueError("Nie umiem zrobic takiej fabryki")
+
+    @abstractmethod
+    def stworz_pojazd(self, model: str, rok: int) -> Pojazd:
+        pass
+
+    @staticmethod
+    def sprawdz_rok(rok: int) -> bool:
+        return 2024 >= rok >= 1900
+
+    def ile_wyprodukowano(self) -> int:
+        return self._liczba_wyprodukowanych
+
+    def _zwieksz_licznik(self):
+        self._liczba_wyprodukowanych += 1
 
 class FabrykaSamochodow(FabrykaPojazdow):
     def stworz_pojazd(self, model: str, rok: int, liczba_drzwi: int = 4) -> Samochod:
-        # tu implementacja
 
-        return pojazd
+        if self.sprawdz_rok(rok):
+            pojazd = Samochod(model, rok, liczba_drzwi)
+            self._zwieksz_licznik()
+            return pojazd
+        raise ValueError("Nieprawidłowy rok produkcji!")
 
 class FabrykaAutobusow(FabrykaPojazdow):
     def stworz_pojazd(self, model: str, rok: int, liczba_miejsc: int = 50) -> Autobus:
-        # tu implementacja
-
-        return pojazd
+        if self.sprawdz_rok(rok):
+            pojazd = Autobus(model, rok, liczba_miejsc)
+            self._zwieksz_licznik()
+            return pojazd
+        raise ValueError("Nieprawidłowy rok produkcji!")
 
 
 def main():
@@ -44,8 +86,8 @@ def main():
     fabryka_autobusow = FabrykaPojazdow.utworz_fabryke('autobus', "Fabryka Autobusów Kraków")
 
     # Utworzone fabryki - demonstracja @property nazwa
-    print(f"Nazwa fabryki: {fabryka_samochodow.nazwa}")  
-    print(f"Nazwa fabryki: {fabryka_autobusow.nazwa}")  
+    print(f"Nazwa fabryki: {fabryka_samochodow.nazwa}")
+    print(f"Nazwa fabryki: {fabryka_autobusow.nazwa}")
 
     # Utworz pojazdy
     samochod = fabryka_samochodow.stworz_pojazd("Fiat", 2023, liczba_drzwi=5)
